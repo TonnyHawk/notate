9b5ed74796a5d2d1edecd35f7ba2cf17bcf3d793
TonnyHawk <anton.veremko@gmail.com>
2021-03-06 14:06:15 +0200

    styles prepared

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 let {gulp, src, dest, parallel, series, watch} = require('gulp'),
    sass = require('gulp-sass'),
-   browserSync = require('browser-sync').create();
+   browserSync = require('browser-sync').create(),
+   autoprefixer = require('gulp-autoprefixer'),
+   gcmq = require('gulp-group-css-media-queries'),
+   rename       = require('gulp-rename'),
+   cleanCSS = require('gulp-clean-css');
 
 let projFold = './src';
 let distFold = './public'
@@ -9,21 +13,29 @@ function startBrowser(){
    browserSync.init({
       server: {
           baseDir: distFold
-      }
+      },
+      notify: false
    });
 }
 
 function css(){
    return src(projFold + '/styles/main.scss')
          .pipe(sass())
+         .pipe(gcmq())
+         .pipe(autoprefixer())
+         .pipe(cleanCSS())
+         .pipe(rename("app-style.min.css"))
          .pipe(dest(distFold + '/css/'))
          .pipe(browserSync.stream())
 }
 
 function watcher(){
    watch(projFold+'/styles/**/*.scss', css)
+   watch(distFold+'/index.html').on('change', browserSync.reload)
 }
 
-let build = parallel(series(css, watcher), startBrowser)
+let develop = parallel(series(css, watcher), startBrowser)
+let build = series(css)
 
-exports.default = build;
+exports.default = develop;
+exports.build = build;
